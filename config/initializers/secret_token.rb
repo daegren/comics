11c9e347f76b5698df1fd88d4e77232dc75b3416
David Mills <dave@1morepx.com>
2013-07-31 20:16:39 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comics::Application.config.secret_key_base = '45892d589d9dff756c842c92a7e286a3b92d15325c4fae4b8323e22dbda3d2cb620a2f780cc72eaad26495b2547a88ef6b870c2c932a5cad2b19c33c5b1a082c'
